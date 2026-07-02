3fe36193639d40f3d481c304195366c4f5b50821
Luis Lopez <llopezl55@miumg.edu.gt>
2026-07-02 11:30:50 -0600

    Creación de página login con bootstrap

--- BlazorApp1/Repositorio/UsuarioRepositorio.cs
+++ BlazorApp1/Repositorio/UsuarioRepositorio.cs
@@ -25,34 +25,56 @@ namespace BlazorApp1.Repositorio
             }
             else
             {
-                usuarioDesdeDb.Nombre = usuario.Nombre;
-                usuarioDesdeDb.Apellido = usuario.Apellido;
-                usuarioDesdeDb.FechaNacimiento = usuario.FechaNacimiento;
-                usuarioDesdeDb.Telefono = usuario.Telefono;
-                usuarioDesdeDb.Correo = usuario.Correo;
-                usuarioDesdeDb.Rol = usuario.Rol;
-                usuarioDesdeDb.Estado = usuario.Estado;
-                // Solo actualizamos la contraseña si se proporciona una nueva
-                if (!string.IsNullOrWhiteSpace(usuario.Contraseña))
+                bool existeCorreo = await _contexto.Usuarios
+                    .AnyAsync(u => u.Correo == usuario.Correo
+                    && u.IdUsuario != idUsuario);
+                if (existeCorreo)
                 {
-                    usuarioDesdeDb.Contraseña = BCrypt.Net.BCrypt.HashPassword(usuario.Contraseña);
+                    throw new Exception("El correo ya está registrado.");
                 }
+                else
+                {
+                    usuarioDesdeDb.Nombre = usuario.Nombre;
+                    usuarioDesdeDb.Apellido = usuario.Apellido;
+                    usuarioDesdeDb.FechaNacimiento = usuario.FechaNacimiento;
+                    usuarioDesdeDb.Telefono = usuario.Telefono;
+                    usuarioDesdeDb.Correo = usuario.Correo;
+                    usuarioDesdeDb.Rol = usuario.Rol;
+                    usuarioDesdeDb.Estado = usuario.Estado;
+                    // Solo actualizamos la contraseña si se proporciona una nueva
+                    if (!string.IsNullOrWhiteSpace(usuario.Contraseña))
+                    {
+                        usuarioDesdeDb.Contraseña = BCrypt.Net.BCrypt.HashPassword(usuario.Contraseña);
+                    }
 
-                await _contexto.SaveChangesAsync();
-                return usuarioDesdeDb;
+                    await _contexto.SaveChangesAsync();
+                    return usuarioDesdeDb;
+                }
+                
             }
             
         }
+
 
         // Implementación del método CrearUsuario
         public async Task<Usuario> CrearUsuario(Usuario usuario)
         {
             if (usuario != null)
             {
-                usuario.Contraseña = BCrypt.Net.BCrypt.HashPassword(usuario.Contraseña);
-                await _contexto.Usuarios.AddAsync(usuario);
-                await _contexto.SaveChangesAsync();
-                return usuario;
+                bool existeCorreo = await _contexto.Usuarios
+                   .AnyAsync(u => u.Correo == usuario.Correo);
+                if (existeCorreo)
+                {
+                    throw new Exception("El correo ya está registrado.");
+                }
+                else
+                {
+                    usuario.Contraseña = BCrypt.Net.BCrypt.HashPassword(usuario.Contraseña);
+                    await _contexto.Usuarios.AddAsync(usuario);
+                    await _contexto.SaveChangesAsync();
+                    return usuario;
+                }
+                    
 
             }
             else
@@ -118,6 +140,31 @@ namespace BlazorApp1.Repositorio
 
                 return usuario;
             }
+        }
+
+        //Implementación del método CambiarContraseña
+        public async Task CambiarContraseña(string correo, string nuevaContraseña)
+        {
+            var usuario = await _contexto.Usuarios
+                .FirstOrDefaultAsync(u => u.Correo == correo);
+
+            if (usuario == null) {
+                throw new Exception("No existe un usuario con ese correo.");
+            }
+            else
+            {
+                if (string.IsNullOrWhiteSpace(nuevaContraseña))
+                {
+                    throw new Exception("La nueva contraseña no puede estar vacía.");
+                }
+                else
+                {
+                    usuario.Contraseña = BCrypt.Net.BCrypt.HashPassword(nuevaContraseña);
+
+                    await _contexto.SaveChangesAsync();
+                }
+            }
+                            
         }
     }
 }
